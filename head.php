40ab0faea719d46a13133b5d2dae8be56fa2771f
VictorMoreira98 <39058177+VictorMoreira98@users.noreply.github.com>
2020-02-17 22:51:22 -0300

    Delete head.php

--- head.php
+++
@@ -1,8 +0,0 @@
-<nav id="navbar">
-        <ul>
-            <img src="teste.png">
-            <li id="titulo"><h1>Manager LO </h1></li>
-            <li id="login"><a href=""><img src="user.png">Login</a></li>
-            
-        </ul>
-</nav>
